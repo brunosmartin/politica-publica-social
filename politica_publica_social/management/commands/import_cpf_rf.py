3f856f2dd9d61bbd6f6210c467929096581daa7f
Laury Bueno <laury@hacklab.com.br>
2017-09-06 17:48:32 -0300

    Users import now takes more scenarios into account and can be used as a standalone importer for new and existing profiles

--- politica_publica_social/management/commands/import_cpf_rf.py
+++ politica_publica_social/management/commands/import_cpf_rf.py
@@ -2,7 +2,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model, models
 from django.db import transaction
+from django.db.utils import IntegrityError
 from politica_publica_social.models import SindsepProfile
+from core.models import Course, Class
+
 import unicodecsv
 import random
 
@@ -10,39 +13,77 @@ User = get_user_model()
 
 collumns = [
     'RF',
+    'cpf',
     'name',
-    'job',
-    'cell',
+    'occupation',
     'email',
 ]
 
 
 class Command(BaseCommand):
     args = 'file'
-    help = 'import users'
+    help = 'import users to a group and class'
 
     @transaction.atomic
-    def handle(self, *files, **options):
+    def handle(self, *args, **options):
 
-        if not len(files) == 1:
-            raise CommandError('No file to import')
+        if not len(args) == 2:
+            raise CommandError('Incorrect parameters')
 
-        group, _ = models.Group.objects.get_or_create(name="Associados")
+        group, _ = models.Group.objects.get_or_create(name="Curso Reflexões")
+        students = models.Group.objects.get(name="students")
+        
+        course = Course.objects.get(name='Reflexões sobre desenvolvimento infantil')
+        classs, _ = Class.objects.get_or_create(name=args[1], course=course)
 
-        with open(files[0], 'r') as csvfile:
+        with open(args[0], 'r') as csvfile:
             readf = unicodecsv.DictReader(csvfile)
             count = 0
             for row in readf:
+                email = row.get('email')
                 cpf = row.get('cpf')
+                cpf_length = len(cpf)
+                if cpf_length < 11:
+                    for i in range(11-cpf_length):
+                        cpf = '0' + cpf
+                
                 try:
-                    user = User.objects.get(username=cpf)
+                    with transaction.atomic():
+                        user = User.objects.create(username=cpf, email=email)
                     profile = SindsepProfile()
-                    profile.user = user
-                    profile.cpf = cpf
-                    profile.rf = row.get('RF')
-                    profile.save()
-                except:
-                    pass
+                    user.is_active = False
+                    print('created ' + user.username)
+                except IntegrityError as e:
+                    user = User.objects.get(username=cpf)
+                    profile = SindsepProfile.objects.get(user=user)
+                    print('found ' + user.username)
+                
+                user.first_name, user.last_name = row.get('name').split(' ', 1 )
+                user.rg = row.get('RF')
+                if user.first_name:
+                    user.first_name = user.first_name.title()
+                if user.last_name:
+                    user.last_name = user.last_name.title()
+                if row.has_key('email') and row.get('email'):
+                    user.email = row.get('email')
+                else:
+                    user.email = str(random.randint(1,100000000)) + '@nomail.com'
+
+                if row.has_key('occupation'):
+                    user.occupation = row.get('occupation')
+
+
+                user.save()
+                
+                profile.user = user
+                profile.cpf = cpf
+                profile.rf = row.get('RF')
+                profile.save()
+            
+                classs.students.add(user)
+                group.user_set.add(user)
+                students.user_set.add(user)
+                
                 count += 1
-                if count % 100 == 0:
+                if count % 10 == 0:
                     self.stdout.write(str(count))
